65425708d895a3387fb7275a7b3918cf61344e8e
Arindrajit Paul <paul_ap07@protonmail.ch>
2022-03-21 14:47:49 +0600

    add business logic to add and save students based on if their email is present

--- src/main/java/com/example/student/student/StudentRepository.java
+++ src/main/java/com/example/student/student/StudentRepository.java
@@ -1,9 +1,14 @@
 package com.example.student.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-  
+
+  @Query("SELECT s FROM Student s WHERE s.email = ?1") // Student is the class and not the db
+  Optional<Student> findStudentByEmail(String email);
 }
