ed11c484e8aa86fe2789c314a0c9ec6ed789bff4
Arindrajit Paul <paul_ap07@protonmail.ch>
2022-03-22 13:43:47 +0600

    add delete and update functionalities

--- src/main/java/com/example/student/student/StudentService.java
+++ src/main/java/com/example/student/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.example.student.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 //this is an example of dependancy injection
 //more for semantics; does the same job as declaring it as a bean/component
@@ -29,5 +31,38 @@ public class StudentService {
       throw new IllegalStateException("email taken");
     }
     studentRepository.save(student);
+  }
+
+  public void deleteStudent(Long studentId) {
+    boolean studentExists = studentRepository.existsById(studentId);
+    if (!studentExists) {
+      throw new IllegalStateException("student with id " + studentId + " not present");
+    }
+    studentRepository.deleteById(studentId);
+  }
+
+  @Transactional
+  public void updateStudent(Long studentId,
+      String name, String email) {
+    Student student = studentRepository.findById(studentId)
+        .orElseThrow(() -> new IllegalStateException("student with id "
+            + studentId + " does not exist"));
+
+    if (name != null &&
+        name.length() > 0 &&
+        !Objects.equals(student.getName(), name)) {
+      student.setName(name);
+    }
+
+    if (email != null &&
+        email.length() > 0 &&
+        !Objects.equals(student.getEmail(), email)) {
+      Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+      if (studentOptional.isPresent()) {
+        throw new IllegalStateException("email taken");
+      }
+      student.setEmail(email);
+    }
   }
 }
